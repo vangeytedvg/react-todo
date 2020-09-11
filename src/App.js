d179b04097b75359b51cb5b00d0474e76398f830
Danny Van Geyte <vangeytedvg@gmail.com>
2020-09-11 13:21:35 +0200

    App.js changes

--- src/App.js
+++ src/App.js
@@ -11,13 +11,59 @@ function App() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [emailError, setEmailError] = useState('');
-  const [passwordError, setPasswordErro] = useState();
+  const [passwordError, setPasswordError] = useState();
   const [hasAccount, setHasAccount] = useState(false);
 
+  /**
+   * Handle Login to firebase
+   */
   const handleLogin = () => {
     firebaseConfig
       .auth()
       .signInWithEmailAndPassword(email, password)
+      .catch((err) => {
+        switch (err.code) {
+          case "auth/invalid-email":
+          case "auth/user-disabled":
+          case "auth/user-not-found":
+            setEmailError(err.message);
+            break;
+          case "auth/wrong-password":
+            setPasswordError(err.message);
+            break;
+        }
+      })
+  }
+
+  /**
+   * Handle Sign Up to firebase
+   */
+  const handleSignUp = () => {
+    firebaseConfig
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch((err) => {
+        switch (err.code) {
+          case "auth/email-already-in-use":
+          case "auth/invalid-email":
+            setEmailError(err.message);
+            break;
+          case "auth/weak-password":
+            setPasswordError(err.message);
+            break;
+        }
+      })
+  }
+
+  /**
+   * Handle logout from firebase
+   */
+  const handleLogout = () => {
+    firebaseConfig.auth().signOut()
+  }
+
+  const authListener = () => {
+    
   }
 
   return (
